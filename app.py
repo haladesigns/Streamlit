3423d3373cc72060d6ef8344db73ae39a921c3f2
Hala <hala.francis@gmail.com>
2024-06-06 21:44:02 -0400

    Added histogram plots + sidebar settings

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+import random
 import altair as alt
 import plotly.express as px
 import plotly.graph_objects as go
@@ -7,23 +8,11 @@ import scipy as sp
 
 
 st.header('Car Ads')
-st.write('Car Ads application. Placeholder.')
-
+st.write('Introductory text here')
 
 # Load the dataset
-file_path = '../vehicles_us.csv'
+file_path = './vehicles_us.csv'
 df = pd.read_csv(file_path)
-
-# Calculating percentage of missing values for each column
-missing_values = df.isnull().sum()
-missing_percentage = ((missing_values / len(df)) * 100).round(2)
-
-# Creating a DataFrame for the raw markdown table
-missing_values_df = pd.DataFrame({
-    'Column': missing_values.index,
-    'Missing Values': missing_values.values,
-    'Percentage of Total (%)': missing_percentage.values
-})
 
 # Fill missing values
 df['model_year'] = df['model_year'].fillna(df['model_year'].median())
@@ -34,8 +23,60 @@ df['paint_color'] = df['paint_color'].fillna('unknown')
 # Drop the 'is_4wd' column
 df = df.drop(columns=['is_4wd'])
 
-fig_hist = px.histogram(df, x='price', nbins=30, title='Distribution of Vehicle Prices')
-#fig_hist.show()
+#--------------------SETTINGS-----------------
+# Choose a template
+templates = ["plotly", "ggplot2", "seaborn", "simple_white", "none"]
+
+st.sidebar.header("Settings")
+my_template = st.sidebar.radio("Choose a template", templates, key="template")
+text_auto= st.sidebar.checkbox("Enable text")
 
 
-st.write(fig_hist)
+#---------------------PLOTS-------------------
+expand_all = False
+expand = st.sidebar.button("Expand All", key="expandall")
+minimize = st.sidebar.button("Minimize")
+
+# Initialize session state
+if 'expand_all' not in st.session_state:
+    st.session_state.expand_all = False
+
+if expand: 
+    st.session_state.expand_all = True
+elif minimize:
+    st.session_state.expand_all = False
+
+# Histogram - Distribution of Vehicle Prices
+fig_hist_price = px.histogram(df, x='price', nbins=30, color='condition', text_auto=text_auto, template=my_template)
+fig_hist_price.update_traces(textposition='outside')
+fig_hist_price.update_layout(title_text='Distribution of Vehicle Prices', title_x=0.35)
+#st.write(fig_hist_price)
+my_histprice_expander = st.expander("Expand Distribution of Vehicle Prices", expanded=True)
+with my_histprice_expander:
+    st.write(fig_hist_price)
+
+# Histogram of Odometer Readings: To visualize the distribution of odometer readings across the dataset.
+fig_hist_odometer = px.histogram(df, x='odometer', nbins=30, color='condition', text_auto=text_auto, template=my_template)
+fig_hist_odometer.update_layout(title_text='Distribution of Odometer Readings', title_x=0.35)
+#st.write(fig_hist_odometer)
+my_histodom_expander = st.expander("Expand Distribution of Odometer Readings", expanded=st.session_state.expand_all)
+with my_histodom_expander:
+    st.write(fig_hist_odometer)
+
+# Histogram of Days Listed: To understand how long vehicles are typically listed for sale.
+fig_hist_days_listed = px.histogram(df, x='days_listed', nbins=30, title='Distribution of Days Listed', color='condition', 
+                                    text_auto=text_auto, template=my_template,)
+fig_hist_days_listed.update_layout(title_text='Distribution of Vehicle Conditions', title_x=0.35)
+#st.write(fig_hist_days_listed)
+my_histdayslisted_expander = st.expander("Expand Distribution of Vehicle Conditions", expanded=st.session_state.expand_all)
+with my_histdayslisted_expander:
+    st.write(fig_hist_days_listed)
+
+# Histogram of Vehicle Conditions: To understand the distribution of different vehicle conditions (categorical histogram).
+fig_hist_condition = px.histogram(df, x='condition', color='condition', text_auto=text_auto, template=my_template, )
+fig_hist_condition.update_layout(title_text='Distribution of Vehicle Conditions', title_x=0.35)
+#st.write(fig_hist_condition)
+my_histcondition_expander = st.expander("Expand Distribution of Vehicle Conditions" if not st.session_state.expand_all else "", expanded=st.session_state.expand_all)
+with my_histcondition_expander:
+    st.write(fig_hist_condition)
+
